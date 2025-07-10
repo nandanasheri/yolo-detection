ca0f4f1f053ee8013e6f695acb1988c9ff74f3c9
nandanasheri <nandanasheri@gmail.com>
2025-07-10 15:26:01 -0500

    model works on backend + fetch

--- client/src/app/upload/page.tsx
+++ client/src/app/upload/page.tsx
@@ -1,8 +1,51 @@
-import Link from "next/link";
+'use client'
 import Image from "next/image";
+import { useState } from 'react';
 
 
 export default function Upload() {
+  const [file, setFile] = useState<File | null>(null);
+  const [error, setError] = useState('');
+  const [uploadedImage, setUploadedImage] = useState(null);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0]
+      if (file) {
+        setFile(file)
+      }
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!file) {
+      setError("No files chosen")
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('image', file)
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000/predict', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.text();
+      if (response.ok) {
+        console.log(data)
+        setError('');
+      } else {
+        setError(data || 'An error occurred during upload.');
+      }
+    } catch (err) {
+      console.log(err)
+      setError('An unexpected error occurred.');
+    }
+  }
+
   return (
     <div className="flex flex-col gap-10 mx-auto max-w-3xl h-[90vh] mt-14 px-4">
       <h1 className="text-head font-bold text-3xl hover:text-offwhite">Upload Your Image</h1>
@@ -15,13 +58,18 @@ export default function Upload() {
                 <li>File size must be less than 5MB</li>
                 <li>Here is a quick sample image</li>
             </ul>
-            <Link href="/upload">
-              <button className="rounded-lg mt-10 self-center text-xl font-text w-fit px-5 py-1 hover:bg-offwhite hover:border-1 hover:text-mainbrown bg-mainbrown text-offwhite hover:cursor-pointer">upload image</button>
-            </Link>
+
+            <form onSubmit={handleSubmit}>
+              <input type="file" accept="image/*" onChange={handleImageChange} className="px-5 py-1 border border-mainbrown rounded-lg hover:bg-mainbrown hover:text-offwhite" />
+              <button type="submit" className="rounded-lg mt-10 self-center text-xl font-text w-fit px-5 py-1 hover:bg-offwhite hover:border-1 hover:text-mainbrown bg-mainbrown text-offwhite hover:cursor-pointer">upload image</button>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+              {uploadedImage && <img src={uploadedImage} alt="Uploaded" width="200" />}
+            </form>
+            
         </div>
 
-        <div>
-            <Image className="rounded-xl" src="/sample-fridge.jpg" alt="sample fridge image" width={500} height={500} />
+        <div className="flex content-center items-center overflow-hidden">
+            <Image className="rounded-xl min-w-full min-h-full" src="/sample-fridge.jpg" alt="sample fridge image" width={500} height={600} />
         </div>
       </div>
     </div>
--- server/app/app.py
+++ server/app/app.py
@@ -1,6 +1,44 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from ultralytics import YOLO
+from flask_cors import CORS
+from PIL import Image
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
+
+with app.app_context():
+    app.predictor = YOLO("trained-yolo.pt")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.files
+    image_file = data["image"]
+    image = Image.open(image_file.stream)
+    objects = set()
+
+    results = app.predictor.predict(image)
+
+    result = results[0]
+    for box in result.boxes:
+        class_id = result.names[box.cls[0].item()]
+        conf = round(box.conf[0].item(), 2)
+        if conf > 0.8:
+            objects.add(class_id)
+
+    annotated_image = Image.fromarray(result.plot()[:,:,::-1])
+     # Encode image to base64
+
+    buffered = BytesIO()
+    annotated_image.save(buffered, format="JPEG")  
+    base64_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    return jsonify({
+        "result": list(objects),
+        "image": base64_image
+    })
+
 
 @app.route('/')
 def hello_world():
